f98d4cc85a297d2e1b1e3ffd63bce388220c5e25
saraalicescebba <saraalice.scebba@studenti.unimi.it>
2022-12-03 17:24:17 +0100

    this works

--- Script/01_HTML_links.R
+++ Script/01_HTML_links.R
@@ -5,7 +5,6 @@ url_beppe_blog <- "https://beppegrillo.it/un-mare-di-plastica-ci-sommergera/"
 download.file(url, destfile = here::here("beppeblog.html"))
 
 r <- read_html(here::here("beppeblog.html")) %>% 
-  html_elements(css = "a") %>% 
-  html_text(trim = TRUE)
+  html_elements(css = "a") 
 
 r
