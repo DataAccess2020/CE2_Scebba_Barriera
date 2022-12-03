55cbda1f7dd88cd41e59f8d5368be974e67dd6ee
gaiabarriera <gaia.barriera@studenti.unimi.it>
2022-12-03 15:27:47 +0100

    RCurl code

--- Script/00_RCurl_code.R
+++ Script/00_RCurl_code.R
@@ -1,2 +1,7 @@
+library(RCurl)
+url <- "https://beppegrillo.it/un-mare-di-plastica-ci-sommergera/" 
+email <- "barrieragaia.uni@gmail.com"
+user_agent <- R.Version()$version.string
 
-curl: :getURL()
+beppe_page <- RCurl::getURL(url=url, httpheader=c(from=email,
+                                                 'User-Agent'= user_agent))
