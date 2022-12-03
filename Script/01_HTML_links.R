879c6bb8888650c5b9c4ea5bf3706cd778c53aa6
gaiabarriera <gaia.barriera@studenti.unimi.it>
2022-12-03 18:03:57 +0100

    Data frame links

--- Script/01_HTML_links.R
+++ Script/01_HTML_links.R
@@ -4,7 +4,7 @@ library(tidyverse)
 url_beppe_blog <- "https://beppegrillo.it/un-mare-di-plastica-ci-sommergera/"
 download.file(url, destfile = here::here("beppeblog.html"))
 
-r <- read_html(here::here("beppeblog.html")) %>% 
+links <- read_html(here::here("beppeblog.html")) %>% 
   html_elements(css = "a") 
 
-r
+links
