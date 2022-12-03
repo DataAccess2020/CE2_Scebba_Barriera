97c3c543c8c889fe422f76b91f2dbe4d2d57f265
gaiabarriera <gaia.barriera@studenti.unimi.it>
2022-12-03 16:13:38 +0100

    HTML post links

--- Script/01_HTML_links.R
+++ Script/01_HTML_links.R
@@ -3,3 +3,9 @@ library(tidyverse)
 
 url_beppe_blog <- "https://beppegrillo.it/"
 download.file(url_beppe_blog, destfile = here::here("beppeblog.html"))
+
+# HTML_FIRST TRY ---------
+
+post_links <- read_html(here::here("beppeblog.html")) %>% 
+  html_elements(css="h3 a")
+
