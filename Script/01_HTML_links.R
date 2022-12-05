51a04251c17e101b22b972871b229a535a3532cb
gaiabarriera <gaia.barriera@studenti.unimi.it>
2022-12-05 10:17:56 +0100

    Merge

--- Script/01_HTML_links.R
+++ Script/01_HTML_links.R
@@ -5,6 +5,4 @@ url_beppe_blog <- "https://beppegrillo.it/un-mare-di-plastica-ci-sommergera/"
 download.file(url, destfile = here::here("beppeblog.html"))
 
 links <- read_html(here::here("beppeblog.html")) %>% 
-  html_elements(css = "a") 
-
-links
+  html_elements(css = "a")
