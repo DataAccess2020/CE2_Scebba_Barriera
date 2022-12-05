a8138facd6c69bc2e53d8fbfe775df5cd68506bc
gaiabarriera <gaia.barriera@studenti.unimi.it>
2022-12-05 12:41:56 +0100

    Download politely function

--- Script/02_page2016.R
+++ Script/02_page2016.R
@@ -3,13 +3,30 @@
 
 require(stringr)
 
-links_2016 <- str_c("https://beppegrillo.it/category/archivio/2016/", 1:47)
+links_2016 <- str_c("https://beppegrillo.it/category/archivio/2016/", 1:47) %>% 
+  paste0("/")
+
 dir.create("post_2016")   
 
 # creating a loop ------
 
-url2 <- ("https://beppegrillo.it/category/archivio/2016/")
+url2 <- ("https://beppegrillo.it/category/archivio/2016/page/")
 
+download.file(url="https://beppegrillo.it/category/archivio/2016", 
+              destfile= here::here ("archive_2016.html"))
+  
+
+download_politely <- function(from_url, to_html , my_email, my_agent)
+
+req <- httr:GET(url=url2,
+                add_headers(
+                  from=email,
+                  'User-Agent'=user_agent
+                ))
+
+download_politely(from_url = url2,
+                  to_html = here::here ("archive_2016.html"),
+                  my_email= email)
 
 for (i in seq_along(links_2016)) {
   cat(i, " ")
@@ -18,3 +35,4 @@ for (i in seq_along(links_2016)) {
                                       'User-Agent'= user_agent))
   Sys.sleep(2)
 }
+
