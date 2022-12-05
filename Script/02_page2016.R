aecb4ee155dc5b3c69afd3b9ab30ab123b86bd2a
saraalicescebba <saraalice.scebba@studenti.unimi.it>
2022-12-05 19:11:57 +0100

    loop doesn't work

--- Script/02_page2016.R
+++ Script/02_page2016.R
@@ -1,41 +1,55 @@
-# Building the full list of links --------
-
-
-require(stringr)
+library(rvest)
+library(XML)
+library(tidyverse)
+library(httr)
 
 links_2016 <- str_c("https://beppegrillo.it/category/archivio/2016/", 1:47) %>% 
   paste0("/")
 
 dir.create("post_2016")   
 
-# creating a loop ------
-
-url2 <- ("https://beppegrillo.it/category/archivio/2016/page/")
 
 download.file(url="https://beppegrillo.it/category/archivio/2016", 
               destfile= here::here ("archive_2016.html"))
+
+
+
+#creating a function download politely
+
+
+download_politely <- function(from_url, to_html , my_email, my_agent=user_agent) 
   
 
-download_politely <- function(from_url, to_html , my_email, my_agent)
+
+url2 <- "https://beppegrillo.it/category/archivio/2016/page/"
 
   
-  
-  
-req <- httr:GET(url=url2,
+req <- httr::GET(url=url2,
                 add_headers(
                   from=email,
                   'User-Agent'=user_agent
                 ))
 
-download_politely(from_url = url2,
-                  to_html = here::here ("archive_2016.html"),
+
+download_politely(from_url=url2,
+                  to_html = here::here("archive_2016.html"),
                   my_email= email)
+
+require(stringr)
+
+links_2016 <- str_c("https://beppegrillo.it/category/archivio/2016/", 1:47) %>% 
+  paste0("/")
+
+dir.create("post_2016")
 
 for (i in seq_along(links_2016)) {
   cat(i, " ")
   
-download_politely(from_url = links_2016[i],
-                  to_html = here::here("post_2016", str_c()))
+  download_politely(from_url=links_2016[i],
+                    to_html = here::here("post_2016", str_c("page_",i,".html")),
+                    my_email= email)
+  
   Sys.sleep(2)
 }
+
 
