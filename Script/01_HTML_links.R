fbcdae2ad11e4ce9f69c8e7f598b3303ae89885e
saraalicescebba <saraalice.scebba@studenti.unimi.it>
2022-12-05 10:13:34 +0100

    creating dataframe

--- Script/01_HTML_links.R
+++ Script/01_HTML_links.R
@@ -5,6 +5,11 @@ url_beppe_blog <- "https://beppegrillo.it/un-mare-di-plastica-ci-sommergera/"
 download.file(url, destfile = here::here("beppeblog.html"))
 
 links <- read_html(here::here("beppeblog.html")) %>% 
-  html_elements(css = "a") 
+  html_elements(css = "a")%>% 
+  html_text(trim = TRUE)
 
-links
+links2 <- as_tibble(links)
+
+library(stringr)
+
+posts_link <- str_view_all(links2, pattern= "https://beppegrillo.it/.")
