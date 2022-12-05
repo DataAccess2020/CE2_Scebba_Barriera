652db201dfd33e95e1b3e94eb3de759990056795
saraalicescebba <saraalice.scebba@studenti.unimi.it>
2022-12-05 19:38:44 +0100

    still doesn't work :(

--- Script/02_page2016.R
+++ Script/02_page2016.R
@@ -3,44 +3,47 @@ library(XML)
 library(tidyverse)
 library(httr)
 
-links_2016 <- str_c("https://beppegrillo.it/category/archivio/2016/", 1:47) %>% 
-  paste0("/")
 
-dir.create("post_2016")   
+url_archive <- "https://beppegrillo.it/category/archivio/2016/page/"
 
 
 download.file(url="https://beppegrillo.it/category/archivio/2016", 
               destfile= here::here ("archive_2016.html"))
 
 
+links_2016 <- str_c("https://beppegrillo.it/category/archivio/2016/", 1:47) 
+links_2016
 
-#creating a function download politely
 
 
-download_politely <- function(from_url, to_html , my_email, my_agent=user_agent) 
+download_politely <- function(from_url, to_html , my_email, my_agent=R.Version()$version.string) {
   
+  stopifnot(is.character(from_url))
+  stopifnot(is.character(to_html))
+  stopifnot(is.character(my_email))
+
+req <- httr::GET(url = url2,
+                 add_headers(
+                   From = email,
+                   `User-Agent` = R.Version()$version.string
+                 )
+                 )
 
 
-url2 <- "https://beppegrillo.it/category/archivio/2016/page/"
-
-  
-req <- httr::GET(url=url2,
-                add_headers(
-                  from=email,
-                  'User-Agent'=user_agent
-                ))
-
+if (httr::http_status(req)$message == "Success: (200) OK") {
+  bin <- content(req, as = "raw")
+  writeBin(object = bin, con = to_html)
+} else {
+  cat("Houston, we have a problem!")
+}
+}
 
 download_politely(from_url=url2,
                   to_html = here::here("archive_2016.html"),
-                  my_email= email)
-
-require(stringr)
-
-links_2016 <- str_c("https://beppegrillo.it/category/archivio/2016/", 1:47) %>% 
-  paste0("/")
+                  my_email= "barrieragaia.uni@gmail.com")
 
 dir.create("post_2016")
+
 
 for (i in seq_along(links_2016)) {
   cat(i, " ")
